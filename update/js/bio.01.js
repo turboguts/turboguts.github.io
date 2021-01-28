990bd057af1da99322e92f6337680f452cae04dc
yoe <58783618+turboguts@users.noreply.github.com>
2021-01-28 19:50:14 +0000

    Add files via upload

--- update/js/bio.01.js
+++ update/js/bio.01.js
@@ -14,30 +14,28 @@ function draw() {
     stroke(128);
     noFill();
 
-translate(canvas.clientWidth/2, height/2)
-
-    beginShape();
-    for (let a = 0; a < TWO_PI; a += 0.1) {
-        let xoff = cos(a) + mouseX/1000;
-        let yoff = sin(a) + mouseY/1000;
-        let r = map(noise(xoff, yoff, zoff), 0, 1, 100, 180);
-        let x = r * cos(a);
-        let y = r * sin(a);
-
-        vertex(x/2, y/2);
-    }
-    endShape(CLOSE);
-
+    translate(canvas.clientWidth / 2, height / 2)
     zoff += 0.005;
     beginShape();
-    for (let a = 0; a < TWO_PI; a += 0.1) {
-        let xoff = cos(a) + mouseX/1000;
-        let yoff = sin(a) + mouseY/1000;
-        let r = map(noise(xoff, yoff, zoff), 0, 1, 100, 250);
+    for (let a = 0; a < TWO_PI; a += 0.5) {
+        let xoff = cos(a) + mouseX / 1000;
+        let yoff = sin(a) + mouseY / 1000;
+        let r = map(noise(xoff, yoff, zoff), 0, 1, 1, 250);
         let x = r * cos(a);
         let y = r * sin(a);
 
-        vertex(x/14, y/14);
+        curveVertex((x / 2), (y / 2));
+    }
+    endShape(CLOSE);
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += 0.5) {
+        let xoff = cos(a) + mouseX / 1000;
+        let yoff = sin(a) + mouseY / 1000;
+        let r = map(noise(xoff, yoff, zoff), 0, 1, 1, 250);
+        let x = r * cos(a);
+        let y = r * sin(a);
+
+        curveVertex((x / 2.5), (y / 2.5));
     }
     endShape(CLOSE);
 
